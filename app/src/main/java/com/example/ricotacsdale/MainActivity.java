1284dd4bb1683410151a67c429a27a50554311b1
RicoEmbrado <embradorico@gmail.com>
2023-10-03 13:23:31 -0700

    Added new background color and custom text. Also added one new button.

--- app/src/main/java/com/example/ricotacsdale/MainActivity.java
+++ app/src/main/java/com/example/ricotacsdale/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.ricotacsdale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titleText;
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = (Button) findViewById(R.id.PlayButton);
+        titleText = (TextView) findViewById(R.id.title_text);
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "fonts/Monday Donuts.ttf");
+        titleText.setTypeface(myCustomFont);
+        playButton.setTypeface(myCustomFont);
+
     }
 }
