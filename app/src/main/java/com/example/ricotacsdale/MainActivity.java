9d0b8193d42732d0619c0f4705294902d3477f3f
RicoEmbrado <embradorico@gmail.com>
2023-10-04 13:24:47 -0700

    Tried to fix pictures.

--- app/src/main/java/com/example/ricotacsdale/MainActivity.java
+++ app/src/main/java/com/example/ricotacsdale/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.ricotacsdale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -25,5 +27,10 @@ public class MainActivity extends AppCompatActivity {
         titleText.setTypeface(myCustomFont);
         playButton.setTypeface(myCustomFont);
         instructionsButton.setTypeface(myCustomFont);
+    }
+
+    public void toInstructions(View v)
+    {
+        startActivity(new Intent(MainActivity.this, Instructions.class));
     }
 }
