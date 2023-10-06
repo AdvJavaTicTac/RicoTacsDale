8cfa19a03dd09bd04d337b3412c79f354e7d7d1c
DaleB34 <dalebolido@gmail.com>
2023-10-05 20:48:43 -0700

    Added Empty Image and onClick Function

--- app/src/main/java/com/example/ricotacsdale/PlayScreen.java
+++ app/src/main/java/com/example/ricotacsdale/PlayScreen.java
@@ -3,12 +3,24 @@ package com.example.ricotacsdale;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PlayScreen extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_screen);
+
+        imageView =findViewById(R.id.r0c0);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {
+                imageView.setImageResource(R.drawable.dale);
+            }
+        });
     }
 }
