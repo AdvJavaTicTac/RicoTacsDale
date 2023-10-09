a9020fb10d591c84c65a525fc49a1cca08c677cc
91112021 <dalebolido@gmail.com>
2023-10-09 16:27:05 -0700

    Can now make multiple buttons have same onClick functionality

--- app/src/main/java/com/example/ricotacsdale/PlayScreen.java
+++ app/src/main/java/com/example/ricotacsdale/PlayScreen.java
@@ -8,26 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class PlayScreen extends AppCompatActivity implements View.OnClickListener{
-
-    Button r0c0 =findViewById(R.id.r0c0);
-    Button r0c1 =findViewById(R.id.r0c1);
-    Button r0c2 =findViewById(R.id.r0c2);
-
-    Button r1c0 =findViewById(R.id.r1c0);
-    Button r1c1 =findViewById(R.id.r1c1);
-    Button r1c2 =findViewById(R.id.r1c2);
-
-    Button r2c0 =findViewById(R.id.r2c0);
-    Button r2c1 =findViewById(R.id.r2c1);
-    Button r2c2 =findViewById(R.id.r2c2);
+public class PlayScreen extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_screen);
 
-
+        Button tile1 = findViewById(R.id.tile1);
+        tile1.setOnClickListener(this);
 
         /* Brainstorm of how to change image of tiles
             Need two different conditions, Dale's turn, and Rico's turn
@@ -39,50 +28,22 @@ public class PlayScreen extends AppCompatActivity implements View.OnClickListene
         //9 tiles in total
         //tiles are counted left to right, top to bottom
         /*
-          e.g   0   1   2
-                3   4   5
-                6   7   8
+          e.g   1   2   3
+                4   5   6
+                7   8   9
          */
 
-        //Each tile has its own ImageView so that they can all share the same onClick function while still having different values
-        ImageView tile0 =findViewById(R.id.r0c0);
-        ImageView tile1 =findViewById(R.id.r0c1);
-        ImageView tile2 =findViewById(R.id.r0c2);
+    }
 
-        ImageView tile3 =findViewById(R.id.r1c0);
-        ImageView tile4 =findViewById(R.id.r1c1);
-        ImageView tile5 =findViewById(R.id.r1c2);
 
-        ImageView tile6 =findViewById(R.id.r2c0);
-        ImageView tile7 =findViewById(R.id.r2c1);
-        ImageView tile8 =findViewById(R.id.r2c2);
-
-        //Giving each tile access to the onClick function
-        tile0.setOnClickListener(this);
-        tile1.setOnClickListener(this);
-        tile2.setOnClickListener(this);
-        tile3.setOnClickListener(this);
-        tile4.setOnClickListener(this);
-        tile5.setOnClickListener(this);
-        tile6.setOnClickListener(this);
-        tile7.setOnClickListener(this);
-        tile8.setOnClickListener(this);
-
-//        imageView.setOnClickListener(this {
-//            public void onClick(View view)
-//            {
-//                imageView.setImageResource(R.drawable.dale);
-//            }
-//        });
+    public void changeSymbol(View v)
+    {
+        Button outBtn = (Button)findViewById(v.getId());
+        outBtn.setText("X");
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId())
-        {
-            case R.id.r0c0:
-                r0c0.setImageResource(R.drawable.dale);
-                break;
-        }
+        changeSymbol(v);
     }
 }
