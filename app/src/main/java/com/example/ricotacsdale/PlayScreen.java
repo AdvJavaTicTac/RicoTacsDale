4884d67669a982250fb2a9811448c331766772d8
DaleB34 <dalebolido@gmail.com>
2023-10-09 20:30:02 -0700

    X's and O's now appear on their own turn!!!!

--- app/src/main/java/com/example/ricotacsdale/PlayScreen.java
+++ app/src/main/java/com/example/ricotacsdale/PlayScreen.java
@@ -2,28 +2,18 @@ package com.example.ricotacsdale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-
 public class PlayScreen extends AppCompatActivity implements View.OnClickListener {
 
+    String turn = "X";
+    //Start off with X outside onCreate because I *THINK* it will mess something up
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_screen);
-
-        Button tile1 = findViewById(R.id.tile1);
-        tile1.setOnClickListener(this);
-
-        /* Brainstorm of how to change image of tiles
-            Need two different conditions, Dale's turn, and Rico's turn
-                have variable called turn?
-            Need to check src/image of the tile being pressed
-                If its the "empty" image then change
-         */
 
         //9 tiles in total
         //tiles are counted left to right, top to bottom
@@ -33,17 +23,58 @@ public class PlayScreen extends AppCompatActivity implements View.OnClickListene
                 7   8   9
          */
 
-    }
+        Button tile1 = findViewById(R.id.tile1);
+        Button tile2 = findViewById(R.id.tile2);
+        Button tile3 = findViewById(R.id.tile3);
+        Button tile4 = findViewById(R.id.tile4);
+        Button tile5 = findViewById(R.id.tile5);
+        Button tile6 = findViewById(R.id.tile6);
+        Button tile7 = findViewById(R.id.tile7);
+        Button tile8 = findViewById(R.id.tile8);
+        Button tile9 = findViewById(R.id.tile9);
 
+        tile1.setOnClickListener(this);
+        tile2.setOnClickListener(this);
+        tile3.setOnClickListener(this);
+        tile4.setOnClickListener(this);
+        tile5.setOnClickListener(this);
+        tile6.setOnClickListener(this);
+        tile7.setOnClickListener(this);
+        tile8.setOnClickListener(this);
+        tile9.setOnClickListener(this);
+
+        /* Brainstorm of how to change image of tiles
+            Need two different conditions, Dale's turn, and Rico's turn
+                have variable called turn?
+            Need to check src/image of the tile being pressed
+                If its the "empty" image then change
+         */
+
+
+
+    }
 
     public void changeSymbol(View v)
     {
         Button outBtn = (Button)findViewById(v.getId());
-        outBtn.setText("X");
+        outBtn.setText(turn);
+
+        if(turn.equals( "X"))
+        {
+            turn = "O";
+        }
+        else
+            turn = "X";
     }
 
     @Override
-    public void onClick(View v) {
-        changeSymbol(v);
+    //Like a toString
+    public void onClick(View v)
+    {
+        Button outBtn = (Button)findViewById(v.getId());
+        if(outBtn.getText().equals("Empty"))
+        {
+            changeSymbol(v);
+        }
     }
 }
